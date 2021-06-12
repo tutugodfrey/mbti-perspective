9a964333a03ccd63f5e3efad90c53b584d60c68f
tutugodfrey <godfrey_tutu@yahoo.com>
2021-06-12 12:16:55 +0100

    PostgreSQL RDS Stack
    - Add stack to create PostgreSQL RDS Database

--- aws-infra-cf/stack-helper.sh
+++ aws-infra-cf/stack-helper.sh
@@ -5,4 +5,5 @@ ENVIRONMENT=dev
 function deploy_all() {
   aws cloudformation deploy --stack-name ${PROJECT_NAME}-${ENVIRONMENT}-vpc --template-file vpc.yml --no-fail-on-empty-changeset;
   aws cloudformation deploy --stack-name ${PROJECT_NAME}-${ENVIRONMENT}-security-group --template-file security-groups.yml --no-fail-on-empty-changeset;
+  aws cloudformation deploy --stack-name mbti-dev-rds --template-file rds-stack.yml --parameter-overrides DBMasterPassword=${DB_PASSWD}; # Get DB_PASSWD from environment
 }
