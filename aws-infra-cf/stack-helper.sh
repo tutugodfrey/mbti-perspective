806ee61825be74274cb06c309154dd39313c303b
tutugodfrey <godfrey_tutu@yahoo.com>
2021-06-12 11:12:36 +0100

    Autoscaling Group
    - Add stack to provision load balancer
    - Add stack to provision autoscaling group for app server
    - Add script to install dependencies and configure server
    - Add a stack to create instance profile and role for ec2

--- aws-infra-cf/stack-helper.sh
+++ aws-infra-cf/stack-helper.sh
@@ -5,5 +5,8 @@ ENVIRONMENT=dev
 function deploy_all() {
   aws cloudformation deploy --stack-name ${PROJECT_NAME}-${ENVIRONMENT}-vpc --template-file vpc.yml --no-fail-on-empty-changeset;
   aws cloudformation deploy --stack-name ${PROJECT_NAME}-${ENVIRONMENT}-security-group --template-file security-groups.yml --no-fail-on-empty-changeset;
-  aws cloudformation deploy --stack-name mbti-dev-rds --template-file rds-stack.yml --parameter-overrides DBMasterPassword=${DB_PASSWD}; # Get DB_PASSWD from environment
+  aws cloudformation deploy --stack-name ${PROJECT_NAME}-${ENVIRONMENT}-rds --template-file rds-stack.yml --parameter-overrides DBMasterPassword=${DB_PASSWD}; # Get DB_PASSWD from environment
+  aws cloudformation deploy --stack-name ${PROJECT_NAME}-${ENVIRONMENT}-loadbalancer --template-file load-balancer.yml --no-fail-on-empty-changeset;
+  aws cloudformation deploy --stack-name ${PROJECT_NAME}-${ENVIRONMENT}-instance-profile --template-file ec2-profile.yml --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset;
+  aws cloudformation deploy --stack-name ${PROJECT_NAME}-${ENVIRONMENT}-app-autoscaler --template-file app-autoscaler.yml --no-fail-on-empty-changeset;
 }
